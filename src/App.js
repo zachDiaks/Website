91632eac61c30d56e932bb9dd48c828f7c431dd1
Zach Diaks <zachryandax@gmail.com>
2025-01-01 12:58:09 -0500

    Remove some cruft

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to poop reload. What is the deal with these chickens??
         </p>
         <a
           className="App-link"

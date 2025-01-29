a7e5df802e5172fdca98e6f698c91676e92a2edd
Zach Diaks <zachryandax@gmail.com>
2025-01-29 18:38:41 -0500

    Add simple layout and image so I can show off!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 import './components/StandardButton'
-import StandardButton from './components/StandardButton';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>
+          Welcome! I'm Zach.
+        </h1>
+        <img
+          src={require('./resources/ZachProfPic.jpg')}
+          alt="Zach"
+          style={{width: 500, height: 500}}
+        />
         <p>
-          Welcome to <code>Zach's Website</code>
+          Find me on <a href="https://www.linkedin.com/in/zachary-diaks/"> LinkedIn</a>
+          <br/>
+          More coming soon!
         </p>
-        <StandardButton></StandardButton>
       </header>
     </div>
   );
